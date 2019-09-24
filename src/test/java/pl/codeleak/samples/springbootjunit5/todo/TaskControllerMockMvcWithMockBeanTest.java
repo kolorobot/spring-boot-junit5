1e25db25b901c0d6f4cde60866f14b6719469923
rafal.borowiec <rafal.borowiec@gmail.com>
2019-09-24 23:27:19 +0200

    Spring Boot test with mocked MVC layer and mocked dependency

---
+++ src/test/java/pl/codeleak/samples/springbootjunit5/todo/TaskControllerMockMvcWithMockBeanTest.java
@@ -0,0 +1,47 @@
+package pl.codeleak.samples.springbootjunit5.todo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class TaskControllerMockMvcWithMockBeanTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskRepository taskRepository;
+
+
+    @Test
+    void findsTaskById() throws Exception {
+        // arrange
+        var taskToReturn = new Task();
+        taskToReturn.setId(1);
+        taskToReturn.setTitle("delectus aut autem");
+        taskToReturn.setCompleted(true);
+        taskToReturn.setUserId(1);
+
+        when(taskRepository.findOne(1)).thenReturn(taskToReturn);
+
+        // act and assert
+        mockMvc.perform(get("/tasks/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":1,\"title\":\"delectus aut autem\",\"userId\":1,\"completed\":true}"));
+    }
+}
