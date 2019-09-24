3f5a54532dd186bbd3186fa9b6698f76c37eed28
rafal.borowiec <rafal.borowiec@gmail.com>
2019-09-24 23:26:13 +0200

    Spring Boot test with web server running on random port with mocked dependency

---
+++ src/test/java/pl/codeleak/samples/springbootjunit5/todo/TaskControllerIntegrationTestWithMockBeanTest.java
@@ -0,0 +1,50 @@
+package pl.codeleak.samples.springbootjunit5.todo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TaskControllerIntegrationTestWithMockBeanTest {
+
+    @LocalServerPort
+    private int port;
+
+    @MockBean
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void findsTaskById() {
+
+        // arrange
+        var taskToReturn = new Task();
+        taskToReturn.setId(1);
+        taskToReturn.setTitle("delectus aut autem");
+        taskToReturn.setCompleted(true);
+        taskToReturn.setUserId(1);
+
+        when(taskRepository.findOne(1)).thenReturn(taskToReturn);
+
+        // act
+        var task = restTemplate.getForObject("http://localhost:" + port + "/tasks/1", Task.class);
+
+        // assert
+        assertThat(task)
+                .extracting(Task::getId, Task::getTitle, Task::isCompleted, Task::getUserId)
+                .containsExactly(1, "delectus aut autem", true, 1);
+    }
+}
